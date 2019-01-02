8794f0cb959f984d882caf74523a623dc8a1ae4d
Lilian Weng <lilian@openai.com>
2019-01-02 00:31:53 -0800

    param refactoring - reinforce

--- playground/policies/reinforce.py
+++ playground/policies/reinforce.py
@@ -1,47 +1,43 @@
 import numpy as np
 import tensorflow as tf
-from playground.policies.base import BaseModelMixin, Policy
+from playground.policies.base import BaseModelMixin, Policy, TrainConfig
 from playground.utils.misc import plot_learning_curve
 from playground.utils.tf_ops import dense_nn
 
 
 class ReinforcePolicy(Policy, BaseModelMixin):
     def __init__(self, env, name, training=True, gamma=0.99,
-                 lr=0.001, lr_decay=0.999, batch_size=32, layer_sizes=None,
-                 baseline=False):
+                 layer_sizes=[32, 32], baseline=False):
         Policy.__init__(self, env, name, training=training, gamma=gamma)
         BaseModelMixin.__init__(self, name)
 
-        self.lr = lr
-        self.lr_decay = lr_decay
-        self.batch_size = batch_size
-        self.layer_sizes = layer_sizes or [32, 32]
+        self.layer_sizes = layer_sizes
         self.baseline = baseline
 
     def act(self, state, **kwargs):
-        return self.sess.run(self.sampled_actions, {self.states: [state]})
+        return self.sess.run(self.sampled_actions, {self.s: [state]})
 
     def build(self):
-        self.learning_rate = tf.placeholder(tf.float32, shape=None, name='learning_rate')
+        self.lr = tf.placeholder(tf.float32, shape=None, name='learning_rate')
 
         # Inputs
-        self.states = tf.placeholder(tf.float32, shape=(None, self.state_dim), name='state')
-        self.actions = tf.placeholder(tf.int32, shape=(None,), name='action')
+        self.s = tf.placeholder(tf.float32, shape=[None] + self.state_dim, name='state')
+        self.a = tf.placeholder(tf.int32, shape=(None,), name='action')
         self.returns = tf.placeholder(tf.float32, shape=(None,), name='return')
 
         # Build network
-        self.pi = dense_nn(self.states, self.layer_sizes + [self.act_size], name='pi_network')
+        self.pi = dense_nn(self.s, self.layer_sizes + [self.act_size], name='pi_network')
         self.sampled_actions = tf.squeeze(tf.multinomial(self.pi, 1))
         self.pi_vars = self.scope_vars('pi_network')
 
         if self.baseline:
             # State value estimation as the baseline
-            self.v = dense_nn(self.states, self.layer_sizes + [1], name='v_network')
+            self.v = dense_nn(self.s, self.layer_sizes + [1], name='v_network')
             self.target = self.returns - self.v  # advantage
 
             with tf.variable_scope('v_optimize'):
                 self.loss_v = tf.reduce_mean(tf.squared_difference(self.v, self.returns))
-                self.optim_v = tf.train.AdamOptimizer(self.learning_rate).minimize(
+                self.optim_v = tf.train.AdamOptimizer(self.lr).minimize(
                     self.loss_v, name='adam_optim_v')
         else:
             self.target = tf.identity(self.returns)
@@ -49,11 +45,11 @@ class ReinforcePolicy(Policy, BaseModelMixin):
         with tf.variable_scope('pi_optimize'):
             self.loss_pi = tf.reduce_mean(
                 tf.stop_gradient(self.target) * tf.nn.sparse_softmax_cross_entropy_with_logits(
-                    logits=self.pi, labels=self.actions), name='loss_pi')
+                    logits=self.pi, labels=self.a), name='loss_pi')
             # self.optim_pi = tf.train.AdamOptimizer(self.lr)
             # self.grads_pi = self.optim_pi.compute_gradients(self.loss_pi, self.pi_vars)
             # self.train_pi_op = self.optim_pi.apply_gradients(self.grads_pi)
-            self.optim_pi = tf.train.AdamOptimizer(self.learning_rate).minimize(
+            self.optim_pi = tf.train.AdamOptimizer(self.lr).minimize(
                 self.loss_pi, name='adam_optim_pi')
 
         with tf.variable_scope('summary'):
@@ -76,15 +72,22 @@ class ReinforcePolicy(Policy, BaseModelMixin):
 
         self.sess.run(tf.global_variables_initializer())
 
-    def train(self, n_episodes, every_episode):
+    class TrainConfig(TrainConfig):
+        lr = 0.001
+        lr_decay = 0.999
+        batch_size = 32
+        n_episodes = 800
+        log_every_episode = 10
+
+    def train(self, config: TrainConfig):
         step = 0
         episode_reward = 0.
         reward_history = []
         reward_averaged = []
 
-        lr = self.lr
+        lr = config.lr
 
-        for n_episode in range(n_episodes):
+        for n_episode in range(config.n_episodes):
             ob = self.env.reset()
             done = False
 
@@ -108,7 +111,7 @@ class ReinforcePolicy(Policy, BaseModelMixin):
             reward_history.append(episode_reward)
             reward_averaged.append(np.mean(reward_history[-10:]))
             episode_reward = 0.
-            lr *= self.lr_decay
+            lr *= config.lr_decay
 
             # Estimate returns backwards.
             return_so_far = 0.0
@@ -120,24 +123,23 @@ class ReinforcePolicy(Policy, BaseModelMixin):
 
             _, summ_str = self.sess.run(
                 [self.train_ops, self.merged_summary], feed_dict={
-                    self.learning_rate: lr,
-                    self.states: np.array(obs),
-                    self.actions: np.array(actions),
+                    self.lr: lr,
+                    self.s: np.array(obs),
+                    self.a: np.array(actions),
                     self.returns: np.array(returns),
                     self.ep_reward: reward_history[-1],
                 })
             self.writer.add_summary(summ_str, step)
 
-            if reward_history and every_episode and n_episode % every_episode == 0:
+            if reward_history and config.log_every_episode and n_episode % config.log_every_episode == 0:
                 # Report the performance every `every_step` steps
                 print("[episodes:{}/step:{}], best:{}, avg:{:.2f}:{}, lr:{:.4f}".format(
-                    n_episode, step, np.max(reward_history),
-                    np.mean(reward_history[-10:]), reward_history[-5:],
-                    lr,
+                    n_episode, step, np.max(reward_history), np.mean(reward_history[-10:]),
+                    reward_history[-5:], lr,
                 ))
-                # self.save_model(step=step)
+                # self.save_checkpoint(step=step)
 
-        self.save_model(step=step)
+        self.save_checkpoint(step=step)
 
         print("[FINAL] episodes: {}, Max reward: {}, Average reward: {}".format(
             len(reward_history), np.max(reward_history), np.mean(reward_history)))
